e22b87109490f11c91c0269ecc7004260363fd76
Yasintha28 <yasinthachamikara28@gmail.com>
2025-04-07 07:41:26 +0530

    get user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -87,6 +87,7 @@ export function loginUser(req,res){
                 res.json({
                     message : "Login successful",
                     token: token,
+                    user : userData,
                 }); 
 
             }else{
